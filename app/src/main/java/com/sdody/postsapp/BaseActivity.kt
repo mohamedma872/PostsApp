758fe65ecffd6a0328bf1da345ceb223c5872c59
Mohammed ElSedody <mohammed.elsdody@egabi.com>
2019-10-21 19:33:06 +0200

    build the project

---
+++ app/src/main/java/com/sdody/postsapp/BaseActivity.kt
@@ -0,0 +1,25 @@
+package com.sdody.postsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.MenuItem
+
+abstract class BaseActivity:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setUpToolbar()
+    }
+
+    private fun setUpToolbar() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/com/sdody/postsapp/Constants.kt
@@ -0,0 +1,9 @@
+package com.sdody.postsapp
+
+object Constants {
+    val API_URL = "http://jsonplaceholder.typicode.com/"
+
+    object Posts {
+        val DB_NAME = "posts_db"
+    }
+}
